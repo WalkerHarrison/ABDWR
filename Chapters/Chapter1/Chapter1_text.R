cce30c3838ca723a34fdfde9d70e1d4fd48fc8cc
Walker Harrison <walkerharrison1000@gmail.com>
2018-04-08 01:56:56 -0400

    git commit -m more chapter 1

--- Chapters/Chapter1/Chapter1_text.R
+++ Chapters/Chapter1/Chapter1_text.R
@@ -1,8 +1,8 @@
 ####### Chapter 1 #######
 library(tidyverse)
 library(retrosheet)
-
-#setwd("/Users/walkerharrison/PROJECT_R/ABDWR/Chapters/Chapter1")
+library(lubridate)
+setwd("/Users/walkerharrison/PROJECT_R/ABDWR/Data")
 
 ## Section 1.2.2
 temp <- tempfile()
@@ -77,34 +77,129 @@ Pitching %>%
             CG = sum(CG)) %>%
   mutate(perc.finish = CG/GS)
 
-## Section 1.3
+## Section 1.3.5
 
+games95 <- getRetrosheet("game", 1995)
+games95 %>% filter(Date == 19950906, 
+                   HmTm == "BAL")
+
+## Section 1.3.6
 games80_17 <- do.call(rbind,
                       lapply(1980:2017, 
                              function(year) getRetrosheet("game", year)))
-
 games80_17 %>% 
-  mutate(month = month(ymd(Date))) %>% 
+  mutate(month = month(ymd(Date), label = TRUE)) %>% 
   group_by(month) %>%
   summarize(HRpG = mean(HmHR + VisHR))
 
+Parks <- read_csv("baseballdatabank-2017.1/core/Parks.csv")
+
 games80_17 %>% 
-  group_by(ParkID) %>%
-  summarize(HRpG = mean(HmHR + VisHR))
+  inner_join(Parks, by = c("ParkID" = "park.key")) %>%
+  group_by(park.name) %>%
+  summarize(games = n(),
+            HRpG = mean(HmHR + VisHR)) %>%
+  filter(games > 100) %>%
+  arrange(desc(HRpG))
 
 games80_17 %>% 
   group_by(UmpHNm) %>%
-  summarize(RpG = mean(HmRuns + VisRuns),
-            total_g = n()) %>%
-  filter(total_g > 400) %>% View()
+  summarize(games = n(),
+            RpG = mean(HmRuns + VisRuns)) %>%
+  filter(games > 400) %>%
+  arrange(desc(RpG))
 
 games80_17 %>% 
-  mutate(day = wday(ymd(Date), label = T)) %>% 
+  mutate(day = wday(ymd(Date), label = TRUE)) %>% 
   group_by(day) %>%
-  summarize(avg_att = mean(Attendance, na.rm = T)) %>% View()
+  summarize(avg_att = mean(Attendance, na.rm = TRUE))
 
 
-## Section 1.3
+## Section 1.4
+setwd('chadwick-0.6.5')
+
+
+NYY01 <- getRetrosheet("play", 2001, "NYA")
+NYY01 %>% head()
+
+parse.retrosheet2.pbp(2001)
+data2001 <- read_csv('download.folder/unzipped/all2001.csv', col_names = FALSE)
+fields <- read_csv('download.folder/unzipped/fields.csv')
+names(data2001) <- fields %>% pull(Header)
+data2001 %>% 
+  filter(GAME_ID == "OAK200110130")
+
+data2001 %>% 
+  filter(AWAY_TEAM_ID == "NYA",
+         PIT_ID == "mussm001") %>%
+  pull(GAME_ID) %>% unique()
+
+
+event_key <- read_csv('download.folder/unzipped/event_key.csv')
+data2017 <- data2017 %>% inner_join(event_key, by = "EVENT_CD")
+
+setwd('chadwick-0.6.5')
+parse.retrosheet2.pbp(2017)
+
+data2017 <- read_csv('download.folder/unzipped/all2017.csv')
+fields <- read_csv('download.folder/unzipped/fields.csv')
+names(data2017) <- fields %>% pull(Header)
+event_key <- read_csv('download.folder/unzipped/event_key.csv')
+data2017 <- data2017 %>% inner_join(event_key, by = "EVENT_CD")
+
+judge17 <- data2017 %>%
+  filter(BAT_ID == 'judga001')
+
+
+
+### Section1.4.3
+
+parse.retrosheet2.pbp(1998)
+data1998 <- read_csv('download.folder/unzipped/all1998.csv', col_names = FALSE)
+names(data1998) <- fields %>% pull(Header)
+
+data1998 %>%
+  inner_join(event_key, by = "EVENT_CD") %>%
+  filter(BAT_ID %in% c('mcgwm001', 'sosas001'),
+         BAT_EVENT_FL == TRUE,
+         (!is.na(BASE1_RUN_ID) | !is.na(BASE2_RUN_ID) | !is.na(BASE3_RUN_ID))) %>%
+  count(BAT_ID, RESULT) %>%
+  group_by(BAT_ID) %>%
+  mutate(n/sum(n)) %>%
+  filter(RESULT == "Home run")
+
+data1998 %>%
+  inner_join(event_key, by = "EVENT_CD") %>%
+  filter(BAT_ID %in% c('mcgwm001', 'sosas001'),
+         BAT_EVENT_FL == TRUE,
+         (!is.na(BASE1_RUN_ID) | !is.na(BASE2_RUN_ID) | !is.na(BASE3_RUN_ID))) %>%
+  count(BAT_ID, RESULT) %>%
+  filter(!RESULT %in% c("Walk", "Intentional walk", "Hit by pitch")) %>%
+  group_by(BAT_ID) %>%
+  mutate(n/sum(n)) %>%
+  filter(RESULT == "Home run")
+
+parse.retrosheet2.pbp(2001)
+data2001 <- read_csv('download.folder/unzipped/all2001.csv', col_names = FALSE)
+names(data2001) <- fields %>% pull(Header)
+
+data2001 %>%
+  inner_join(event_key, by = "EVENT_CD") %>%
+  filter(BAT_ID == "bondb001",
+         RESULT == "Intentional walk") %>%
+  group_by(FIRST = !is.na(BASE1_RUN_ID), 
+           SECOND = !is.na(BASE2_RUN_ID), 
+           THIRD = !is.na(BASE3_RUN_ID)) %>%
+  summarize(IBB = n())
+
+parse.retrosheet2.pbp(2011)
+data2001 <- read_csv('download.folder/unzipped/all2011.csv', col_names = FALSE)
+names(data2011) <- fields %>% pull(Header)
+
+data2011  %>%
+  filter(PITCH_SEQ_TX == "BBX",
+         AB_FL == TRUE) %>%
+  summarize(mean(H_FL>0))
 
 teams_17 <- getTeamIDs(2017)
 
@@ -113,3 +208,8 @@ full_17 <- lapply(teams_17, function(team) getRetrosheet("play", 2017, team))
 teams_98
 
 getRetrosheet("play", 2017, "NYA")
+
+
+teams_98 <- getTeamIDs(1998)
+
+full_17 <- lapply(teams_17, function(team) getRetrosheet("play", 2017, team))
